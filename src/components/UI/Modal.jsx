50c05239e1c8bf2e23405333a4942570d3427299
dev-marian-vybyranyi <marian.vybyranyi.dev@gmail.com>
2025-12-31 10:32:17 +0200

    add portal-based Modal component utilizing the native dialog element

---
+++ src/components/UI/Modal.jsx
@@ -0,0 +1,21 @@
+import { useEffect, useRef } from "react";
+import { createPortal } from "react-dom";
+
+const Modal = ({ children, open, className = "" }) => {
+  const dialogRef = useRef(null);
+
+  useEffect(() => {
+    if (open) {
+      dialogRef.current.showModal();
+    }
+  }, [open]);
+
+  return createPortal(
+    <dialog ref={dialogRef} className={`modal ${className}`}>
+      {children}
+    </dialog>,
+    document.getElementById("modal")
+  );
+};
+
+export default Modal;
